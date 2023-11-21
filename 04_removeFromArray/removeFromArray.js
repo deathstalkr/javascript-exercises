9a9e3b31758b9fb2be2069f89281f136570df1f2
deathstalkr <solomon.johnson098@gmail.com>
2023-11-22 01:20:28 +0530

    remove from array Logic added

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...elements) {
+    return arr.filter(item => !elements.includes(item));
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
