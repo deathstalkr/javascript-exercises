ed733a6854c3877f7f811700d7be33feaf873746
deathstalkr <solomon.johnson098@gmail.com>
2023-11-21 20:37:00 +0530

    Reverse string logic

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let string = str;
+    let arr = Array.from(string);
+    let revString = '';
+    for (let i = arr.length-1; i>=0; i--){
+        revString += arr[i]; 
+    }
+    return revString;
 };
 
 // Do not edit below this line
