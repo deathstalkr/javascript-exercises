8f514f91a1ed9cc9a64e12080f01ee5e576de0be
deathstalkr <solomon.johnson098@gmail.com>
2023-11-21 19:53:17 +0530

    repeateString logic and test update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let string = '';
+    if (num>=0){
+        for (let i = 0; i < num; i++){
+            string += str
+        }
+    } else {
+        return 'ERROR';
+    }
+    return string;
 };
 
 // Do not edit below this line
