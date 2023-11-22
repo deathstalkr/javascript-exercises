537588e6ca67c69ca6497e955ae37d605abcc7ab
deathstalkr <solomon.johnson098@gmail.com>
2023-11-22 11:13:41 +0530

    sumAll logic added

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+	if (min < 0 || max < 0) {
+		return `ERROR`;
+	} else if (!(typeof min === "number") || !(typeof max === "number")) {
+		return `ERROR`;
+	} else if (min > max) {
+		temp = min;
+		min = max;
+		max = temp;
+		// [min, max] = [max, min]; // swap values using array destructuring
+	}
+	let sum = 0;
+	for (let i = min; i <= max; i++) {
+		sum += i;
+	}
 
+	return sum;
 };
 
 // Do not edit below this line
