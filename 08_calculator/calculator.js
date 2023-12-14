989301f9db221ba86ce654d8c1dfa8212b2253e8
deathstalkr <solomon.johnson098@gmail.com>
2023-12-14 13:05:24 +0530

    added calculator logic and update test suit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+	return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+	if (number == 0) {
+		return 1;
+	}
+	let factorial = 1;
+	for (let i = number; i > 0; i--) {
+		factorial *= i;
+	}
+	return factorial;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
