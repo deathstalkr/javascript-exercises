adeb340d38f1b71e46821056320316c0824021dc
deathstalkr <solomon.johnson098@gmail.com>
2023-12-15 11:43:18 +0530

    added palindrome logic and updated test suit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	transform = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    return transform.split("").reverse().join("") == transform;
+    
+    // conventional method
+	// length = transform.length - 1;
+	// for (let i = 0; i <= length / 2; i++) {
+	// 	if (!(transform[i] == transform[length - i])) {
+	// 		return false;
+	// 	}
+	// }
+	// return true;
 };
 
 // Do not edit below this line
