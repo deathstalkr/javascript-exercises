31fa24abd3f27db063b9e77d5697c3b1481d096a
deathstalkr <solomon.johnson098@gmail.com>
2023-12-16 22:26:26 +0530

    added fibonacci logic and update test suit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (sequence) {
+	if (sequence <= 0) return "OOPS";
+	if (sequence == 0) return 0;
 
+	let current = 1;
+	let previous = 0;
+
+	for (let i = 2; i <= sequence; i++) {
+		let intermediate = current;
+		current = current + previous;
+		previous = intermediate;
+	}
+
+	return current;
 };
 
 // Do not edit below this line
