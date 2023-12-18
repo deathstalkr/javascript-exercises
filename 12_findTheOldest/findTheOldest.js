c1482589467e5529ecf58da6bdb9c6d2a21f5d6c
deathstalkr <solomon.johnson098@gmail.com>
2023-12-18 11:28:46 +0530

    added logic for findTheOldest and modified tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function (object) {
+	currentYear = new Date().getFullYear();
+	sorted = object.sort((firstPerson, secondPerson) => {
+		if (
+			firstPerson.yearOfDeath == null ||
+			secondPerson.yearOfDeath == null
+		) {
+			if (
+				currentYear - firstPerson.yearOfBirth >
+				currentYear - secondPerson.yearOfBirth
+			) {
+				return -1;
+			} else {
+				return 1;
+			}
+		} else if (
+			firstPerson.yearOfDeath - firstPerson.yearOfBirth >
+			secondPerson.yearOfDeath - secondPerson.yearOfBirth
+		) {
+			return -1;
+		} else {
+			return 1;
+		}
+	});
 
+	return sorted[0];
 };
 
 // Do not edit below this line
